722d147357c79e7e58c7c67ae21aba8d4a08eed8
matthew rendely <matthew.rendely@gmail.com>
2023-03-10 15:07:10 -0800

    add simple css transition

--- new-tab-page/script.js
+++ new-tab-page/script.js
@@ -1,6 +1,6 @@
 //Define how far back in history to pull from for highly visited sites
 //And how many to show on the page
-topNHistorySites = 25;
+topNHistorySites = 45;
 historyTimeRange = Date.now() - 4 * 7 * 24 * 60 * 60 * 1000; // 4 weeks
 
 (async () => {
@@ -10,13 +10,16 @@ historyTimeRange = Date.now() - 4 * 7 * 24 * 60 * 60 * 1000; // 4 weeks
     addSitesToSection(topSites, 'topSites');
 
     //Then get sites from history, sort by most visited and add the top 20 to the page
+    //TODO: can't rely on typedCount field, also should pull in bookmark clicks auto_bookmark
+    //TODO: will probably need linked type as well but with some filtering since sites
+    //like reddit create a lot of junk duplicates
     const historicSites = await chrome.history.search(
-      { text: "", startTime: historyTimeRange, maxResults: 500 });
+      { text: "", startTime: historyTimeRange, maxResults: 10000 });
 
     //Sort by most visisted
     const historicSitesSortedByVisitCount = historicSites.sort((a, b) => {
-      if (a.visitCount > b.visitCount) return -1;
-      if (a.visitCount < b.visitCount) return 1;
+      if (a.typedCount > b.typedCount) return -1;
+      if (a.typedCount < b.typedCount) return 1;
       return 0;
     });
 
@@ -25,9 +28,12 @@ historyTimeRange = Date.now() - 4 * 7 * 24 * 60 * 60 * 1000; // 4 weeks
     for (s of historicSitesSortedByVisitCount) {
       const v = await chrome.history.getVisits({ url: s.url });
       //must be typed transition type
-      if (v.map(i => i.transition === 'typed').includes(true) &&
+      if (v.map(i => i.transition === 'typed').includes(true) 
+      // &&
       // remove if found in a topSite already
-      !topSites.some(t => s.url.match(t.url))) historicSitesFiltered.push(s);
+      // TODO: improve deduplication with some regex cleanup
+      // !topSites.some(t => s.url.match(t.url))
+      ) historicSitesFiltered.push(s);
     }
 
     //TODO: sort by frecency score
@@ -38,7 +44,7 @@ historyTimeRange = Date.now() - 4 * 7 * 24 * 60 * 60 * 1000; // 4 weeks
     // });
 
     //Add history shortcuts to page
-    addSitesToSection(historicSitesFiltered.splice(0, topNHistorySites), 'topSites');
+    addSitesToSection(historicSitesFiltered.splice(0, topNHistorySites), 'calcedTopSites');
 
   } catch (error) {
     console.error(error);
@@ -51,7 +57,7 @@ function addSitesToSection(topSites, sectionId) {
   const div = document.getElementById(sectionId);
   for (site of topSites) {
     const siteShortcut = document.createElement('div');
-    siteShortcut.className = 'top-site';
+    siteShortcut.className = 'top-site animate';
     const link = document.createElement('a');
     link.href = site.url;
     link.className = 'top-site-link';
