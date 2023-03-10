8d199bd600d23647eecd71530f28d377dec15aa3
matthew rendely <matthew.rendely@gmail.com>
2023-03-09 23:18:42 -0800

    add top 20 sites from history

--- new-tab-page/script.js
+++ new-tab-page/script.js
@@ -1,24 +1,43 @@
+//Define how far back in history to pull from for highly visited sites
+//And how many to show on the page
+topNHistorySites = 20;
+historyTimeRange = Date.now() - 4 * 7 * 24 * 60 * 60 * 1000; // 4 weeks
+
 //Get the top sites and add them to the page
-chrome.topSites.get(showTopSites);
+chrome.topSites.get()
+  .then(topSites => addSitesToSection(topSites,'topSites'))
 
+  //Then get sites from history, sort by most visited and add the top 20
+  //to the page
+  .then(chrome.history.search({text: "", startTime: historyTimeRange})
+    .then(historicSites =>  {
+      historicSitesSorted = historicSites.sort((a,b) => {
+        if (a.visitCount > b.visitCount) return -1;
+        if (a.visitCount < b.visitCount) return 1;
+        return 0;
+      });
+      addSitesToSection(historicSitesSorted.splice(0,topNHistorySites),'topSites');
+    })
+  );
 
-//Add them to the page
-function showTopSites(topSites){
-  console.log(topSites);
-  const div = document.getElementById('topSites');
+//Add site shortcuts to the page
+function addSitesToSection(topSites,sectionId){
+  const div = document.getElementById(sectionId);
   for (site of topSites){
-    console.log(site);
-    console.log(site.url);
     const siteShortcut = document.createElement('div');
+    siteShortcut.className = 'top-site';
     const link = document.createElement('a');
     link.href = site.url;
-    link.textContent = site.title;
-    siteShortcut.className = 'top-site';
+    link.className = 'top-site-link';
+    const title = document.createElement('span');
+    if (site.visitCount) title.textContent += `[${site.visitCount}] `
+    title.textContent += site.title;
     // siteShortcut.textContent = site.title + ' - ' + site.url;
     const favicon = document.createElement('img');
-    favicon.src = `https://www.google.com/s2/favicons?sz=64&domain=${site.url}`
+    if (site.url.match('http')) favicon.src = `https://www.google.com/s2/favicons?sz=64&domain=${site.url}`
+    div.appendChild(link);
+    link.appendChild(siteShortcut);
     siteShortcut.appendChild(favicon);
-    siteShortcut.appendChild(link);
-    div.appendChild(siteShortcut);
+    siteShortcut.appendChild(title);
   }
 }
