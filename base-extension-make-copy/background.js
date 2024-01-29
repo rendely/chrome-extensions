b2aaae431b13c2189e928dfd4a0a7e8e28b559af
matthew rendely <matthew.rendely@gmail.com>
2024-01-29 14:15:12 -0800

    Add tabGroup exmaples to base extension

--- base-extension-make-copy/background.js
+++ base-extension-make-copy/background.js
@@ -7,3 +7,33 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.action.setBadgeText({"text": ""} );
   }, 2000);
 });
+
+chrome.tabGroups.onUpdated.addListener( function(group){
+  console.log(group)
+}
+)
+chrome.bookmarks.getTree(
+  results => console.log(results)
+)
+
+chrome.bookmarks.create(
+  {title: 'My bookmarks folder'}
+)
+
+chrome.bookmarks.search(
+  'matthew.com', (node) => {console.log(node)}
+)
+
+chrome.tabs.onUpdated.addListener(
+  (tabId, changeInfo, tab) => {
+  //  if (changeInfo.status === 'complete' || changeInfo.groupId !== undefined)
+    console.log(tabId, changeInfo, tab)
+    chrome.tabs.query({}, function(tabs){console.log(tabs)})
+  }
+)
+
+chrome.tabs.onMoved.addListener(
+  (tabId, moveInfo)=> {
+    console.log(moveInfo)
+  }
+)
