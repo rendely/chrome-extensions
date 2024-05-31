a4cf7d5135cb04626a24b6c1671fa5048c3fb5dd
matthew rendely <matthew.rendely@gmail.com>
2024-05-30 22:02:05 -0700

    Clean up unused boilerplate, add side panel

--- suggest-next-sites/README.md
+++ suggest-next-sites/README.md
@@ -1,12 +1,3 @@
-# Base Chrome Extension
+# Suggest Next Sites
 
-## About
-
-Simple base version of a Chrome Extension to make it faster to prototype new ones.
-Cute dog icon as placeholder
-
-## Useful links
-
-- Chrome Extensions Documentation: https://developer.chrome.com/docs/extensions/reference/
-- Example extensions: https://github.com/GoogleChrome/chrome-extensions-samples 
-- Extension reloader: https://chrome.google.com/webstore/detail/extensions-reloader/fimgfedafeadlieiabdeeaodndnlbhid 
+Chrome extension to suggest the next site to visit based on your history.
--- suggest-next-sites/background.js
+++ suggest-next-sites/background.js
@@ -1,39 +1,4 @@
-// Run when extension loads
-chrome.runtime.onInstalled.addListener(function () {
-  console.log('Updating badge');
-  chrome.action.setBadgeText({"text": "Update"} );
-  //timeout function
-  setTimeout(function(){    
-    chrome.action.setBadgeText({"text": ""} );
-  }, 2000);
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.sidePanel.setOptions({ path: 'sidepanel.html' });
+  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 });
-
-chrome.tabGroups.onUpdated.addListener( function(group){
-  console.log(group)
-}
-)
-chrome.bookmarks.getTree(
-  results => console.log(results)
-)
-
-chrome.bookmarks.create(
-  {title: 'My bookmarks folder'}
-)
-
-chrome.bookmarks.search(
-  'matthew.com', (node) => {console.log(node)}
-)
-
-chrome.tabs.onUpdated.addListener(
-  (tabId, changeInfo, tab) => {
-  //  if (changeInfo.status === 'complete' || changeInfo.groupId !== undefined)
-    console.log(tabId, changeInfo, tab)
-    chrome.tabs.query({}, function(tabs){console.log(tabs)})
-  }
-)
-
-chrome.tabs.onMoved.addListener(
-  (tabId, moveInfo)=> {
-    console.log(moveInfo)
-  }
-)
