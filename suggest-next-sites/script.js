0b45074995fab071501a7be61dd5a60af7f2643b
matthew rendely <matthew.rendely@gmail.com>
2024-05-30 22:32:09 -0700

    Add in first 10 recent history

--- suggest-next-sites/script.js
+++ suggest-next-sites/script.js
@@ -1,19 +1,37 @@
-
 chrome.tabs.query({active: true, currentWindow: true},
     tabs => updateCurrent(tabs[0])
 );
 
 chrome.tabs.onActivated.addListener(refreshSuggest)
 
+getAllHistory();
+
 async function refreshSuggest(info){
     const tabId = info.tabId;
     const tab = await chrome.tabs.get(tabId);
-    updateCurrent(tab)
-
+    updateCurrent(tab);
 }
 
 function updateCurrent(tab){
     const current = document.getElementById('current');
     current.href = tab.url;
     current.innerText = tab.title;
+}
+
+async function getAllHistory(){
+    const all_history = await chrome.history.search(
+        {text: "", maxResults: 10000, startTime: 0});
+    showSites(all_history.slice(0,10));
+}
+
+function showSites(sites){
+    const sites_list = document.getElementById('sites');
+    sites.forEach(s => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = s.href;
+        a.innerText = s.title;
+        li.appendChild(a);
+        sites_list.appendChild(li)
+    })
 }
