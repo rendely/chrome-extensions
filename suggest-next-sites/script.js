b86926fd85a7fe3f757237921138bef983e38d69
matthew rendely <matthew.rendely@gmail.com>
2024-06-01 12:37:48 -0700

    Fix suggestions function

--- suggest-next-sites/script.js
+++ suggest-next-sites/script.js
@@ -1,65 +1,69 @@
-currentTabId = undefined;
-currentUrl = undefined;
+var currentTabId = 'not set';
+var currentUrl = 'not set';
 
-chrome.tabs.query({active: true, currentWindow: true},
+chrome.tabs.query({ active: true, currentWindow: true },
     tabs => updateCurrent(tabs[0])
 );
+
+function updateCurrent(tab) {
+    currentTabId = tab.id;
+    currentUrl = tab.url;
+    const current = document.getElementById('current');
+    current.href = tab.url;
+    current.innerText = tab.title;
+    getSuggested();
+}
 
 chrome.tabs.onActivated.addListener(info => refreshSuggest(info.tabId))
 
 chrome.tabs.onUpdated.addListener((tabId, info) => {
-    if (tabId === currentTabId) refreshSuggest(tabId) });
+    if (tabId === currentTabId) refreshSuggest(tabId)
+});
 
-getSuggested();
 
-async function refreshSuggest(tabId){
+async function refreshSuggest(tabId) {
     currentTabId = tabId;
     const tab = await chrome.tabs.get(tabId);
     currentUrl = tab.url;
     updateCurrent(tab);
 }
 
-function updateCurrent(tab){
-    currentTabId = tab.id;
-    currentUrl = tab.url;
-    const current = document.getElementById('current');
-    current.href = tab.url;
-    current.innerText = tab.title;
-}
 
-async function getSuggested(){
+async function getSuggested() {
+    console.log('getting suggested for:', currentUrl);
     const allHistory = await chrome.history.search(
-        {text: "", maxResults: 10000, startTime: 0});
+        { text: "", maxResults: 10000, startTime: 0 });
 
-    const allVisits = []
-    
-    allHistory.forEach(async h => {
-        const visits = await chrome.history.getVisits({url: h.url});
-        const url_visits = visits.map(v => ({url: h.url, time: v.visitTime}))
-        allVisits.push(...url_visits)
+    const allVisitsPromises = allHistory.map(async h => {
+        const visits = await chrome.history.getVisits({ url: h.url });
+        return visits.map(v => ({ url: h.url, time: v.visitTime }));
     });
 
-    const sortedVisits = allVisits.sort((a,b) => b.time - a.time)
+    const allVisitsArrays = await Promise.all(allVisitsPromises);
+    const allVisits = allVisitsArrays.flat();
+
+
+    const sortedVisits = allVisits.sort((a, b) => a.time - b.time)
     
     const nextCount = {}
-    for (let i = 0; i < sortedVisits.length -1; i++){
-        if (sortedVisits[i].url === currentUrl){
-            const nextUrl = allHistory[i+1].url 
-            console.log(nextUrl);
-            if (nextCount[nextUrl]){
+    for (let i = 0; i < sortedVisits.length - 1; i++) {
+        if (sortedVisits[i].url === currentUrl) {
+            const nextUrl = sortedVisits[i + 1].url
+            if (nextCount[nextUrl]) {
                 nextCount[nextUrl]++
-            }else{
+            } else {
                 nextCount[nextUrl] = 1
             }
         }
     }
-    const results = Object.keys(nextCount);
-    showSites(results.slice(0,10));
+    const sortedNext = Object.entries(nextCount).sort((a,b) => b[1] - a[1]);
+    const results = sortedNext.map(s => s[0]);
+    showSites(results.slice(0, 10));
 }
 
-function showSites(sites){
-    console.log(sites);
+function showSites(sites) {
     const sitesList = document.getElementById('sites');
+    sitesList.innerHTML = '';
     sites.forEach(s => {
         const li = document.createElement('li');
         const a = document.createElement('a');
