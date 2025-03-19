69bef42a00c0c2c3ae7b4168406146df2c83c949
matthew rendely <matthew.rendely@gmail.com>
2025-03-18 23:01:31 -0700

    Fixed aspect ratio

--- auto-zoom/content.js
+++ auto-zoom/content.js
@@ -17,20 +17,21 @@
       frame.style.left = "0";
       frame.style.width = "100%";
       frame.style.height = "100%";
-      frame.style.padding = "5vw"; // Adjust border size
+      // frame.style.padding = "5vw"; // Adjust border size
       frame.style.boxSizing = "border-box";
       frame.style.background = "linear-gradient(45deg, rgb(188 184 255) 0%, rgb(70 53 130) 35%, rgb(90 112 222) 100%)"; // Color of the border
       frame.style.overflow = "hidden";
+      frame.style.display = "flex";
       
 
       
       let content = document.createElement("div");
       content.style.overflow = "auto";
       content.style.margin = "auto";
-      // content.style.maxHeight = "95vh";
-      // content.style.aspectRatio = "1"; 
+      content.style.maxHeight = "95vh";
+      content.style.aspectRatio = "1"; 
       // content.style.width = "100%";
-      content.style.height = "100%";
+      // content.style.height = "100%";
       content.style.backgroundColor = "lightgray"; // Background of the page
       content.style.borderRadius = "15px"; // Rounded corners inside the frame
       content.style.boxShadow = "black 0 0 30px"
